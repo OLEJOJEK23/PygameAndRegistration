e0df494d84d6c9a19af87ac902f2e74b9471bfd1
bashmachenkov313 <bashmachenkovoleg@yandex.ru>
2024-06-15 05:16:38 +0300

    дописал тесты

--- tests/test_Interface.py
+++ tests/test_Interface.py
@@ -7,10 +7,10 @@ import os
 @pytest.mark.parametrize(
     "login, password, result, path",
     [
-        ('test', 'test', True, os.path.abspath(os.path.join(os.path.abspath(os.getcwd()),'..'))),
-        ('test', 'test1', False, os.path.abspath(os.path.join(os.path.abspath(os.getcwd()),'..'))),
-        ('test1234', 'test', False, os.path.abspath(os.path.join(os.path.abspath(os.getcwd()),'..'))),
-        ('test1234', 'test12312', False, os.path.abspath(os.path.join(os.path.abspath(os.getcwd()),'..')))
+        ('test', 'test', True, os.path.abspath(os.path.join(os.path.abspath(os.getcwd()), '..'))),
+        ('test', 'test1', False, os.path.abspath(os.path.join(os.path.abspath(os.getcwd()), '..'))),
+        ('test1234', 'test', False, os.path.abspath(os.path.join(os.path.abspath(os.getcwd()), '..'))),
+        ('test1234', 'test12312', False, os.path.abspath(os.path.join(os.path.abspath(os.getcwd()), '..')))
     ]
 )
 def test_login_check(login: str, password: str, result: bool, path):
